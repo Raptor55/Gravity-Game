11cad7d3ebd7bc27fea8fbbfbe90e04e52fa9c4b
SidhKulkarni <sidhk@live.com>
2016-10-26 13:31:47 -0700

    Update AndroidLauncher.java

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -12,5 +12,6 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new MyGdxGame(), config);
+		system.out.println;
 	}
 }
